ea685aadfd15d26c7710e3169e4cd21aad8f2527
aaronkipkirui <aaronkipkirui015@gmail.com>
2026-04-18 14:38:24 +0300

    CRUD Rest API with PERN stck

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import pool from "../src/config/db.js"
+import userRoutes from "../src/routes/userRoutes.js"
+import errorHandling from "./middlewares/errorHandler.js"
+import createUserTable from "./data/createUserTable.js"
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api", userRoutes);
+
+// Error handling
+app.use(errorHandling);
+
+// create user table before starting server\
+createUserTable();
+
+// postgres connection
+app.get("/", async(req, res) => {
+    console.log("Start");
+    const result = await pool.query("SELECT current_database()");
+    console.log("End");
+    res.send(`The database name is: ${result.rows[0].current_database}`);
+})
+
+// Server
+app.listen(port, ()=> {
+    console.log(`Server is running on http:localhost: ${port}`);
+})
